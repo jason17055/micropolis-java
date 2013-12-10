fd65c5eb9b194e18ac9c57ffd5a7a9f22e2e750d
Jason Long <jason@long.name>
2013-12-10 16:24:28 -0500

    train: simplify logic in moveImpl

--- src/micropolisj/engine/TrainSprite.java
+++ src/micropolisj/engine/TrainSprite.java
@@ -50,47 +50,50 @@ public class TrainSprite extends Sprite
 		}
 		x += Dx[this.dir];
 		y += Dy[this.dir];
-		if (city.acycle % 4 == 0) {
-			// should be at the center of a cell, if not, correct it
-			x = (x/16) * 16 + TRA_GROOVE_X;
-			y = (y/16) * 16 + TRA_GROOVE_Y;
-			int d1 = city.PRNG.nextInt(4);
-			for (int z = d1; z < d1 + 4; z++) {
-				int d2 = z % 4;
-				if (this.dir != DIR_NONE) { //impossible?
-					if (d2 == (this.dir + 2) % 4)
-						continue;
-				}
+		if (city.acycle % 4 != 0) {
+			// continue present course
+			return;
+		}
 
-				int c = getChar(this.x + Cx[d2], this.y + Cy[d2]);
-				if (((c >= RAILBASE) && (c <= LASTRAIL)) || //track?
-					(c == RAILVPOWERH) ||
-					(c == RAILHPOWERV))
-				{
-					if ((this.dir != d2) && (this.dir != DIR_NONE)) {
-						if (this.dir + d2 == 3)
-							this.frame = FRAME_NW_SE;
-						else
-							this.frame = FRAME_SW_NE;
-					}
-					else {
-						this.frame = TrainPic2[d2];
-					}
-
-					if ((c == RAILBASE) || (c == (RAILBASE+1))) {
-						//underwater
-						this.frame = FRAME_UNDERWATER;
-					}
-					this.dir = d2;
-					return;
-				}
+		// should be at the center of a cell, if not, correct it
+		x = (x/16) * 16 + TRA_GROOVE_X;
+		y = (y/16) * 16 + TRA_GROOVE_Y;
+		int d1 = city.PRNG.nextInt(4);
+		for (int z = d1; z < d1 + 4; z++) {
+			int d2 = z % 4;
+			if (this.dir != DIR_NONE) { //impossible?
+				if (d2 == (this.dir + 2) % 4)
+					continue;
 			}
-			if (this.dir == DIR_NONE) {
-				// train has nowhere to go, so retire
-				this.frame = 0;
+
+			int c = getChar(this.x + Cx[d2], this.y + Cy[d2]);
+			if (((c >= RAILBASE) && (c <= LASTRAIL)) || //track?
+				(c == RAILVPOWERH) ||
+				(c == RAILHPOWERV))
+			{
+				if ((this.dir != d2) && (this.dir != DIR_NONE)) {
+					if (this.dir + d2 == 3)
+						this.frame = FRAME_NW_SE;
+					else
+						this.frame = FRAME_SW_NE;
+				}
+				else {
+					this.frame = TrainPic2[d2];
+				}
+
+				if ((c == RAILBASE) || (c == (RAILBASE+1))) {
+					//underwater
+					this.frame = FRAME_UNDERWATER;
+				}
+				this.dir = d2;
 				return;
 			}
-			this.dir = DIR_NONE;
 		}
+		if (this.dir == DIR_NONE) {
+			// train has nowhere to go, so retire
+			this.frame = 0;
+			return;
+		}
+		this.dir = DIR_NONE;
 	}
 }
