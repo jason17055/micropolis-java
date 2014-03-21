f25d6b96ffd55f0c5f0ef9dac772abbd9f7a541b
Tom Br <tom@tombr.de>
2014-03-21 15:49:58 +0100

    better visit and path drawing in overlay

--- src/micropolisj/gui/OverlayMapView.java
+++ src/micropolisj/gui/OverlayMapView.java
@@ -120,8 +120,8 @@ public class OverlayMapView extends JComponent
 	static final Color VAL_VERYPLUS  = new Color(0x00e600);
 	static final Color VAL_MINUS     = new Color(0xff7f00);
 	static final Color VAL_VERYMINUS = new Color(0xffff00);
-    static final Color PATHCOLOR     = new Color(0x000000);
-    static final Color VISITCOLOR     = new Color(0xffffff);
+    static final Color PATHCOLOR     = new Color(0xE300FF);
+    static final Color VISITCOLOR     = new Color(0x00A9FF);
 
 	private Color getCI(int x)
 	{
@@ -172,7 +172,7 @@ public class OverlayMapView extends JComponent
             Iterator pathPoints = curPath.iterator();
             while(pathPoints.hasNext()){
                 l = (CityLocation) pathPoints.next();
-                maybeDrawRect(gr, PATHCOLOR, l.x,l.y, 10,10);
+                maybeDrawRect(gr, PATHCOLOR, l.x,l.y, 3,3);
             }
         }
 
@@ -185,7 +185,10 @@ public class OverlayMapView extends JComponent
                 if(visits > 0){
                     System.out.println("draw visit at: " + visitloc.x + ", " + visitloc.y);
                     // getCI determines color, see above. so i multiply with 100 to get some colors
-                    maybeDrawRect(gr, getCI(visits * 100), visitloc.x, visitloc.y, 20, 20);
+                    CityLocation middle = new CityLocation(engine.clamp(visitloc.x - 3,0, getWidth()), engine.clamp(visitloc.y - 3,0, getWidth()));
+                    if(engine.onMap(middle)){
+                        maybeDrawRect(gr, VISITCOLOR, middle.x, middle.y, 6, 6);
+                    }
                 }
             }
 
