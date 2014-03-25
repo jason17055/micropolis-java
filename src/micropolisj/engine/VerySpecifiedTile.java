f880444008fe722192a280d326fbce69131a5e5f
Tony Schwedek <schwtony@mi.fu-berlin.de>
2014-03-25 12:19:43 +0100

    freeze bug hopefully fixed

--- src/micropolisj/engine/VerySpecifiedTile.java
+++ src/micropolisj/engine/VerySpecifiedTile.java
@@ -1,6 +1,7 @@
 package micropolisj.engine;
+import java.lang.Comparable;
 
-public class VerySpecifiedTile{
+public class VerySpecifiedTile implements Comparable<VerySpecifiedTile>{
 	CityLocation loc;
 	int roadType;
 	int value;
@@ -14,6 +15,41 @@ public class VerySpecifiedTile{
 		this.loc=r.loc;
 		this.roadType=r.roadType;
 		this.value=value;
+	}
+	public boolean equals(Object o){
+		if(o instanceof VerySpecifiedTile){
+			VerySpecifiedTile v = (VerySpecifiedTile)o;
+			return this.loc.x==v.loc.x&&this.loc.y==v.loc.y&&this.roadType==v.roadType&&this.value==v.value;
+		}
+		return false;
+	}
+	
+	public int compareTo(VerySpecifiedTile b){
+		if(this.equals(b)){
+			return 0;
+		}
+		if(this.loc.x < b.loc.x){
+			return -1;
+		}
+		if(this.loc.x > b.loc.x){
+			return 1;
+		}
+		if(this.loc.y<b.loc.y){
+			return -1;
+		}
+		if(this.loc.y>b.loc.y){
+			return 1;
+		}
+		if(this.roadType<b.roadType){
+			return -1;
+		}
+		if(this.roadType>b.roadType){
+			return 1;
+		}
+		if(this.value<b.value){
+			return -1;
+		}
+		return 1;
 	}
 
 }
