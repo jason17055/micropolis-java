8c850197fe7565795680bd3f4ff638907a87645d
Tom Br <tom@tombr.de>
2014-03-26 13:38:13 +0100

    updated gpl header might missed some. but should be good for now

--- src/micropolisj/engine/techno/StreetUpgradeTech.java
+++ src/micropolisj/engine/techno/StreetUpgradeTech.java
@@ -1,3 +1,11 @@
+// This file is part of DiverCity
+// DiverCity is based on MicropolisJ
+// Copyright (C) 2014 Arne Roland, Benjamin Kretz, Estela Gretenkord i Berenguer, Fabian Mett, Marvin Becker, Tom Brewe, Tony Schwedek, Ullika Scholz, Vanessa Schreck for DiverCity
+//
+// DiverCity is free software; you can redistribute it and/or modify
+// it under the terms of the GNU GPLv3, with additional terms.
+// See the README file, included in this distribution, for details.
+
 package micropolisj.engine.techno;
 
 public class StreetUpgradeTech extends GeneralTechnology {
